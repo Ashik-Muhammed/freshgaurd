6ad958acaf290a2fd2da18fe94aa6a770c2468a1
Ashik-Muhammed <83230423+Ashik-Muhammed@users.noreply.github.com>
2024-08-13 22:31:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-import random
 import cv2
 import numpy as np
 import requests
 import torch
 from PIL import Image
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify, Response, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='.')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
 detected_fruits = []
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+
+# Create the database
+with app.app_context():
+    db.create_all()
 
 
 def check_spoilage(cropped_image):
@@ -109,61 +124,8 @@ def detect_freshness(cropped_image, class_name):
             freshness = freshness_values[3]
         else:
             freshness = freshness_values[4]
-    elif class_name == 'sandwich':
-        if avg_hue < 20:
-            freshness = freshness_values[0]
-        elif avg_hue < 80:
-            freshness = freshness_values[1]
-        elif avg_hue < 140:
-            freshness = freshness_values[2]
-        elif avg_hue < 180:
-            freshness = freshness_values[3]
-        else:
-            freshness = freshness_values[4]
-    elif class_name == 'donut':
-        if avg_hue < 20:
-            freshness = freshness_values[0]
-        elif avg_hue < 80:
-            freshness = freshness_values[1]
-        elif avg_hue < 140:
-            freshness = freshness_values[2]
-        elif avg_hue < 180:
-            freshness = freshness_values[3]
-        else:
-            freshness = freshness_values[4]
-    elif class_name == 'cake':
-        if avg_hue < 20:
-            freshness = freshness_values[0]
-        elif avg_hue < 80:
-            freshness = freshness_values[1]
-        elif avg_hue < 140:
-            freshness = freshness_values[2]
-        elif avg_hue < 180:
-            freshness = freshness_values[3]
-        else:
-            freshness = freshness_values[4]
-    elif class_name == 'hot dog':
-        if avg_hue < 20:
-            freshness = freshness_values[0]
-        elif avg_hue < 80:
-            freshness = freshness_values[1]
-        elif avg_hue < 140:
-            freshness = freshness_values[2]
-        elif avg_hue < 180:
-            freshness = freshness_values[3]
-        else:
-            freshness = freshness_values[4]
-    elif class_name == 'pizza':
-        if avg_hue < 20:
-            freshness = freshness_values[0]
-        elif avg_hue < 80:
-            freshness = freshness_values[1]
-        elif avg_hue < 140:
-            freshness = freshness_values[2]
-        elif avg_hue < 180:
-            freshness = freshness_values[3]
-        else:
-            freshness = freshness_values[4]
+
+
     elif class_name == 'carrot':
         if avg_hue < 20:
             freshness = freshness_values[0]
@@ -186,6 +148,7 @@ def detect_freshness_webcam():
         ret, frame = cap.read()
         if not ret:
             break
+
         results = model(Image.fromarray(frame))
         labels = results.names
         pred_boxes = results.xyxy[0][:, :4]
@@ -195,16 +158,7 @@ def detect_freshness_webcam():
         for label, bbox in zip(results.xyxy[0][:, -1].int(), pred_boxes):
             label = int(label)
             class_name = labels[label]
-            if class_name in ['apple',
-                              'banana',
-                              'orange',
-                              'sandwich',
-                              'broccoli',
-                              'carrot',
-                              'hot dog',
-                              'pizza',
-                              'donut',
-                              'cake']:
+            if class_name in ['apple', 'banana', 'orange', 'broccoli', 'carrot']:
                 bbox = bbox.tolist()
                 cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
                 freshness_score = detect_freshness(cropped_image, class_name)
@@ -213,30 +167,57 @@ def detect_freshness_webcam():
                 detection = {
                     'label': class_name,
                     'freshness': freshness_score,
-                    'is_spoiled': is_spoiled}
+                    'is_spoiled': is_spoiled
+                }
                 detections.append(detection)
-
                 detected_fruits.append(class_name)
 
-        city = 'Delhi'
-        country = 'India'
-        api_key = "f596234f7b2eb18b4a28194da7d158a4"
-        weather_url = requests.get(
-            f'http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city},{country}&units=imperial')
-        weather_data = weather_url.json()
-        temp = round(weather_data['main']['temp'])
-        temp = int((temp - 32) * 5 / 9)
-        humidity = weather_data['main']['humidity']
-        gas_sensor_reading = random.randint(0, 100)
+        esp32_url = 'http://192.168.137.131:8080/sensors'
+        try:
+            response = requests.get(esp32_url)
+            sensor_data = response.json()
+            temp = round(sensor_data['temperature'])
+            humidity = round(sensor_data['humidity'])
+            gas_sensor_reading = sensor_data['gas_sensor']
 
-        if detections:
-            response = jsonify(
-                {'detections': detections, 'temperature': temp, 'humidity': humidity, 'gas_sensor': gas_sensor_reading})
-            return Response(response.data, mimetype='application/json')
+            if detections:
+                response = jsonify({
+                    'detections': detections,
+                    'temperature': temp,
+                    'humidity': humidity,
+                    'gas_sensor': gas_sensor_reading
+                })
+                return Response(response.data, mimetype='application/json')
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching sensor data from ESP32: {e}")
+            return Response(f"Error fetching sensor data from ESP32: {e}", status=500)
 
     cap.release()
     cv2.destroyAllWindows()
     return Response('No detections found', mimetype='text/plain')
+
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    user_list = [{"username": user.username, "password": user.password} for user in users]
+    return jsonify({"users": user_list}), 200
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if User.query.filter_by(username=username).first() is not None:
+        return jsonify({"message": "User already exists!"}), 400
+
+    new_user = User(username=username, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User registered successfully!"}), 201
 
 
 @app.route('/detected_fruits', methods=['GET'])
@@ -247,5 +228,3 @@ def get_detected_fruits():
 
 if __name__ == "__main__":
     app.run(debug=False, host='192.168.137.1', port=5001)
-
-get_detected_fruits()
